cb9c6d74100c3fcd2caa90c95d914061099bfe21
hl3470 <hl3470@naver.com>
2024-10-01 14:57:25 +0900

    feature: Member 엔티티 구현

---
+++ backend/src/main/java/aiin/backend/domains/member/MemberRole.java
@@ -0,0 +1,15 @@
+package aiin.backend.domains.member;
+
+import lombok.Getter;
+
+@Getter
+public enum MemberRole {
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    private final String value;
+
+    MemberRole(String value) {
+        this.value = value;
+    }
+}
