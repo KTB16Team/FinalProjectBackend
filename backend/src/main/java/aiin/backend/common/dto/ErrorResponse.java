d52e20eba4578cb87c19271d60695715c89721e7
mng990 <ryoo0504@gmail.com>
2024-10-01 17:32:53 +0900

    refactor: 1. ErrorResponse 생성자 중 reasons가 전달되지 않는 케이스 추가

--- backend/src/main/java/aiin/backend/common/dto/ErrorResponse.java
+++ backend/src/main/java/aiin/backend/common/dto/ErrorResponse.java
@@ -18,11 +18,24 @@ public class ErrorResponse extends BaseResponse {
 	private final List<String> reasons;
 	private final String code;
 
-	private ErrorResponse(HttpStatus status, String message, List<String> reasons, String code) {
+	private ErrorResponse(
+		HttpStatus status,
+		String message,
+		String code,
+		List<String> reasons
+	) {
 		super(status);
 		this.message = message;
 		this.reasons = reasons;
 		this.code = code;
+	}
+
+	public static ErrorResponse from(ErrorCode errorCode) {
+		HttpStatus status = errorCode.getHttpStatus();
+		String message = errorCode.getMessage();
+		String code = errorCode.getCode();
+
+		return new ErrorResponse(status, message, code, null);
 	}
 
 	public static ErrorResponse of(ErrorCode errorCode, List<String> reasons) {
@@ -30,7 +43,7 @@ public class ErrorResponse extends BaseResponse {
 		String message = errorCode.getMessage();
 		String code = errorCode.getCode();
 
-		return new ErrorResponse(status, message, reasons,code);
+		return new ErrorResponse(status, message, code, reasons);
 	}
 
 }
