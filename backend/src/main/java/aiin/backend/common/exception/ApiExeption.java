6c7024c58c84df6b915ef2eaac89414fb888688d
mng990 <ryoo0504@gmail.com>
2024-10-01 15:40:24 +0900

    feat: Api Exception 생성

---
+++ backend/src/main/java/aiin/backend/common/exception/ApiExeption.java
@@ -0,0 +1,20 @@
+package aiin.backend.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiExeption extends RuntimeException {
+    private final HttpStatus httpStatus;
+    public ApiExeption(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+    public static ApiExeption from(ErrorCode errorCode){
+        return new ApiExeption(errorCode.getHttpStatus(), errorCode.getMessage());
+    }
+
+    public static ApiExeption of(HttpStatus httpStatus, String message){
+        return new ApiExeption(httpStatus, message);
+    }
+}
