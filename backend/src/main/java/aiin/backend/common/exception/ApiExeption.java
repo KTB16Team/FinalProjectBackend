421ef4636f72b8431552f33b5cfb9a0544af4ae7
mng990 <ryoo0504@gmail.com>
2024-10-01 16:49:10 +0900

    refactor: 1. API Exception에 Code 필드 추가 2. 생성자 수정

--- backend/src/main/java/aiin/backend/common/exception/ApiExeption.java
+++ backend/src/main/java/aiin/backend/common/exception/ApiExeption.java
@@ -7,17 +7,19 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class ApiExeption extends RuntimeException {
 	private final HttpStatus httpStatus;
+	private final String code;
 
-	public ApiExeption(HttpStatus httpStatus, String message) {
+	public ApiExeption(HttpStatus httpStatus, String message, String code) {
 		super(message);
 		this.httpStatus = httpStatus;
+		this.code = code;
 	}
 
 	public static ApiExeption from(ErrorCode errorCode) {
-		return new ApiExeption(errorCode.getHttpStatus(), errorCode.getMessage());
+		return new ApiExeption(errorCode.getHttpStatus(), errorCode.getMessage(), errorCode.getCode());
 	}
 
-	public static ApiExeption of(HttpStatus httpStatus, String message) {
-		return new ApiExeption(httpStatus, message);
+	public static ApiExeption of(HttpStatus httpStatus, String message, String code) {
+		return new ApiExeption(httpStatus, message, code);
 	}
 }
