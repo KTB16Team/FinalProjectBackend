c53d55a5de8577a55daef08e939ddf5628d3c71a
hl3470 <hl3470@naver.com>
2024-10-02 14:27:16 +0900

    feat: cors 설정 구현

---
+++ backend/src/main/java/aiin/backend/common/cors/CorsConfig.java
@@ -0,0 +1,34 @@
+package aiin.backend.common.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import aiin.backend.common.properties.properties.CorsProperties;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+	private final CorsProperties corsProperties;
+
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSources() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(corsProperties.getAllowedOrigins());
+		config.setAllowedMethods(corsProperties.getAllowedMethods());
+		config.setAllowCredentials(corsProperties.getRequireCredential());
+		config.setAllowedHeaders(corsProperties.getAllowedHeaders());
+		config.setMaxAge(3600L); //1시간
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		// 허용 url 등록
+		corsProperties.getAllowedUrls()
+			.forEach(url -> source.registerCorsConfiguration(url, config));
+
+		return source;
+	}
+}
